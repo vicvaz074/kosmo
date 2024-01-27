fc832445022525c5e5caccf7157f7c04255d8ce3
Vicente Vázquez <128206250+vicvaz074@users.noreply.github.com>
2024-01-27 02:12:09 -0600

    Add files via upload

--- src/LoginComponent.js
+++ src/LoginComponent.js
@@ -1,7 +1,10 @@
-// LoginComponent.js
 import React from 'react';
+import './LoginComponent.css'; 
 import loginVideo from './assets/videos/LOGIN.mp4';
-
+import kosmoBasic from './assets/img/KOSMO_BASICO.svg';
+import lock from './assets/img/CANDADO.svg';
+import loginCircleForKosmoBasic from './assets/img/CIRCULO_LOGIN.svg';
+import loginBackgroundContainer from './assets/img/FONDO_LOGIN_CONTENEDOR.svg';
 
 const LoginComponent = () => {
   return (
