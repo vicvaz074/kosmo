3ecd342e673ef726f3ad3f5e427e88fd2b0d7949
Vicente Vázquez <128206250+vicvaz074@users.noreply.github.com>
2024-02-09 22:33:59 -0800

    Add files via upload

--- src/KosmoModalBot.js
+++ src/KosmoModalBot.js
@@ -7,7 +7,7 @@ const KosmoModalBot = ({ onClose }) => {
     <div className="modalBackdrop" onClick={onClose}>
       <div className="modalContent" onClick={e => e.stopPropagation()}>
         <KosmoTryComponent />
-        <button onClick={onClose} className="closeModalButton">Cerrar</button>
+        <button onClick={onClose} className="closeModalButton">X</button>
       </div>
     </div>
   );
